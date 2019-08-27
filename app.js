0b7b6aed7f298bd6c37cda45e90b9def953d4e55
Pierre Thiollent <pierre.thiollent76@gmail.com>
2019-08-27 19:19:08 +0200

    Refactoring du code, ajout de la balle dans le jeu, déplacement de la balle et gestion des collisions de la balle dans le monde

--- app.js
+++ app.js
@@ -31,15 +31,21 @@ let keyA, keyQ, keyUp, keyDown;
 // Fonction de préchargement du jeu
 function preload() {
     // On charge l'image du paddle
-    this.load.image('paddle', 'img/paddle.png');
+    this.load.image('paddle', 'img/paddle.png')
+
+    // On charge l'image de la balle
+    this.load.image('ball', 'img/ball.png')
+
 }
 
 
 // Fonction de chargement du jeu
 function create() {
-    // On appelle la fonction createPaddle afin d'afficher le paddle du joueur 1 a gauche et du joueur 2 a droite
-    player1 = createPaddle(this, 'LEFT');
-    player2 = createPaddle(this, 'RIGHT');
+    // On instancie deux nouveaux joueurs : un a gauche et un a droite
+    player1 = new Player(this, 'LEFT');
+    player2 = new Player(this, 'RIGHT');
+
+    ball = new Ball(this);
 
     // On définit les touches pour déplacer les paddle
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -53,39 +59,86 @@ function create() {
 function update() {
     // Si la touche A est enfoncée
     if (keyA.isDown) {
-        // On fait monter la paddle
-        player1.setVelocity(0, -100);
+        // On fait monter le paddle
+        player1.toTop();
         // Sinon si la touche Q est enfoncée
     } else if (keyQ.isDown) {
         // On fait descendre le paddle
-        player1.setVelocity(0, 100);
+        player1.toBottom();
     }
     // Si la touche Haut est enfoncée
     if (keyUp.isDown) {
         // On fait monter le paddle
-        player2.setVelocity(0, -100);
+        player2.toTop();
         // Sinon si la touche Bas est enfoncée
     } else if (keyDown.isDown) {
         // On fait descendre le paddle
-        player2.setVelocity(0, 100);
+        player2.toBottom();
     }
 }
 
-// Fonction définissant la position x du paddle droit ou gauche
-function createPaddle(self, side) {
-    let x;
-    let y = game.canvas.height / 2
-
-    // Si le coté est left
-    if (side == 'LEFT') {
-        // On calcule la position x en divisant la largeur de l'image par 2
-        x = PADDLE.width / 2;
-        // Sinon si le coté est droit
-    } else if (side == 'RIGHT') {
-        // On calcule la position x en soustrayant la taille du paddle/2 a la largeur du canvas
-        x = game.canvas.width - PADDLE.width / 2;
+class Player {
+    // On initialise notre objet paddle
+    constructor(self, side) {
+        this.paddle = this.createPaddle(self, side);
     }
-    return self.physics.add.image(x, y, 'paddle');
+
+    // Méthode définissant la position x du paddle droit ou gauche
+    createPaddle(self, side) {
+        let x;
+        let y = game.canvas.height / 2
+
+        // Si le coté est left
+        if (side == 'LEFT') {
+            // On calcule la position x en divisant la largeur de l'image par 2
+            x = PADDLE.width / 2;
+            // Sinon si le coté est droit
+        } else if (side == 'RIGHT') {
+            // On calcule la position x en soustrayant la taille du paddle/2 a la largeur du canvas
+            x = game.canvas.width - PADDLE.width / 2;
+        }
+
+        let paddle = self.physics.add.image(x, y, 'paddle');
+        paddle.body.collideWorldBounds = true;
+
+        return paddle;
+    }
+
+    // Methode gerant la montée du paddle
+    toTop() {
+        this.paddle.setVelocity(0, -PADDLE.speed);
+    }
+
+    // Methode gerant la descente du paddle
+    toBottom() {
+        this.paddle.setVelocity(0, PADDLE.speed);
+    }
+
+}
+
+
+class Ball {
+    // On initialise notre objet ball
+    constructor(self) {
+        this.ball = this.createBall(self);
+        this.init();
+    }
+
+    // Méthode définissant la position x et y de la balle
+    createBall(self) {
+        let x = game.canvas.width / 2;
+        let y = game.canvas.height / 2;
+
+        let ball = self.physics.add.image(x, y, 'ball');
+        ball.body.collideWorldBounds = true;
+        ball.setBounce(1);
+
+        return ball;
+    }
+
+    init() {
+        this.ball.setVelocity(BALL.speed, BALL.speed);
+    }
 }
 
 
